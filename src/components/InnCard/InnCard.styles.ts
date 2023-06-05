172a1b14d1bd082eb89fe593a9a079bbe3104167
Loreto <loreto@guud.tv>
2023-06-05 15:42:27 +0200

    :fire: delete caligari labels from inn card

--- src/components/InnCard/InnCard.styles.ts
+++ src/components/InnCard/InnCard.styles.ts
@@ -1,5 +1,4 @@
-import { theme } from 'theme/theme'
-import { Box, Typography, styled } from '@mui/material'
+import { Box, styled } from '@mui/material'
 
 export const CardMediaWrapper = styled(Box)`
   position: relative;
@@ -19,44 +18,8 @@ export const LabelRatingWrapper = styled(Box)(
   `
 )
 
-export const CardInfoWrapper = styled(Box, {
-  shouldForwardProp: (prop) => prop !== 'isExtraInfoVisible',
-})<{ isExtraInfoVisible?: boolean }>(({ theme, isExtraInfoVisible }) => ({
-  display: 'grid',
-  gridTemplateRows: `min-content ${isExtraInfoVisible ? '1fr' : '0fr'}`,
-  transition: 'grid-template-rows 0.75s',
-  padding: theme.spacing(2),
-}))
-
-export const CardMainInfoWrapper = styled(Box)`
-  position: relative;
-`
-
-export const CardExtraInfoWrapper = styled(Box)`
-  overflow: hidden;
-`
-
-export const CaligariLabelsWrapper = styled(Box)(
+export const CardInfoWrapper = styled(Box)(
   ({ theme }) => `
-    display: flex;
-    flex-wrap: wrap;
-    gap:${theme.spacing(1)};
-    padding: ${theme.spacing(3)} 0;
-  `
-)
-
-export const CaligariLabelsText = styled('span')(
-  ({ theme }) => `
-  display: flex;
-  align-items: center;
-  font-weight: 700;
-  font-size: ${theme.spacing(3)};
-  ::after {
-    content: '';
-    height: 1px;
-   background-color: ${theme.palette.primary.main};
-    flex-grow: 1;
-    margin-left:${theme.spacing(4)};
-  }
+  padding: ${theme.spacing(2)};
 `
 )
--- src/components/InnCard/InnCard.tsx
+++ src/components/InnCard/InnCard.tsx
@@ -1,25 +1,12 @@
-import { Card, CardMedia, Rating, Typography, Tooltip, Fab, useTheme } from '@mui/material'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import { useToggle } from '@/hooks/useToggle'
+import { Card, CardMedia, Rating, Typography } from '@mui/material'
 import { Inn } from '@/models/Inn.model'
-import {
-  CardInfoWrapper,
-  CardMediaWrapper,
-  CaligariLabelsWrapper,
-  LabelRatingWrapper,
-  CardExtraInfoWrapper,
-  CardMainInfoWrapper,
-  CaligariLabelsText,
-} from './InnCard.styles'
-import { CaligariLabel } from '../CaligariLabel/CaligariLabel'
+import { CardInfoWrapper, CardMediaWrapper, LabelRatingWrapper } from './InnCard.styles'
 
 interface InnCardProps {
   inn: Inn
 }
 export const InnCard: React.FC<InnCardProps> = ({ inn }) => {
-  const theme = useTheme()
-  const { image, name, rating, description, address, city, features } = inn
-  const { isVisible: isExtraInfoVisible, toggle } = useToggle()
+  const { image, name, rating, address, city } = inn
   return (
     <Card sx={{ transition: 'all 0.3s 0s ease-in' }}>
       <CardMediaWrapper>
@@ -28,37 +15,13 @@ export const InnCard: React.FC<InnCardProps> = ({ inn }) => {
           <Rating defaultValue={rating} size="small" />
         </LabelRatingWrapper>
       </CardMediaWrapper>
-      <CardInfoWrapper isExtraInfoVisible={isExtraInfoVisible}>
-        <CardMainInfoWrapper>
-          <Typography variant="body1" fontWeight="bold">
-            {name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary" mb={2}>
-            {`${address}, ${city}`}
-          </Typography>
-          <Tooltip arrow title="Mostrar más">
-            <Fab
-              onClick={toggle}
-              size="small"
-              color="secondary"
-              aria-label="Mostrar más"
-              sx={{ position: 'absolute', right: '5%', bottom: '10%' }}
-            >
-              <ExpandMoreIcon />
-            </Fab>
-          </Tooltip>
-        </CardMainInfoWrapper>
-        <CardExtraInfoWrapper>
-          <Typography variant="body2" color="text.secondary" mb={2}>
-            {description}
-          </Typography>
-          <CaligariLabelsText>Etiquetas Caligari</CaligariLabelsText>
-          <CaligariLabelsWrapper>
-            {features.map((feature) => (
-              <CaligariLabel key={feature.id} caligariLabel={feature} />
-            ))}
-          </CaligariLabelsWrapper>
-        </CardExtraInfoWrapper>
+      <CardInfoWrapper>
+        <Typography variant="body1" fontWeight="bold">
+          {name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary" mb={2}>
+          {`${address}, ${city}`}
+        </Typography>
       </CardInfoWrapper>
     </Card>
   )
