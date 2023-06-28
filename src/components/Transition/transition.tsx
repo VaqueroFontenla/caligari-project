320a4a0d04f7ec3e626ab4e133be5ec074f1c660
Loreto <loreto@guud.tv>
2023-06-28 21:50:22 +0200

    :dizzy: Add inns list transition

--- src/components/Transition/transition.tsx
+++ src/components/Transition/transition.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import { Slide } from '@mui/material'
-import { TransitionProps } from '@mui/material/transitions'
+import React, { FC } from 'react'
+import { Slide, SlideProps } from '@mui/material'
 
-export const Transition = React.forwardRef(function Transition(
-  props: TransitionProps & {
-    children: React.ReactElement<any, any>
-  },
-  ref: React.Ref<unknown>
-) {
-  return <Slide direction="up" ref={ref} {...props} timeout={1000} />
-})
+interface TranstionProps {
+  transtionIn: boolean
+  direction?: SlideProps['direction']
+  children: React.ReactElement<any, any>
+}
+
+export const Transition: FC<TranstionProps> = ({ transtionIn, direction, children }) => (
+  <Slide
+    direction={direction || 'right'}
+    in={transtionIn}
+    mountOnEnter
+    unmountOnExit
+    timeout={1000}
+  >
+    <div>{children}</div>
+  </Slide>
+)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,12 +9,6 @@ export default function Home() {
         <meta name="description" content="Guía de los más selectos 'bares/qué lugares'" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-          crossOrigin=""
-        />
       </Head>
       <HomePage />
     </>
